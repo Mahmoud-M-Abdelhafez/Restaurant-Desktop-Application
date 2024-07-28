35057485271cb67d625378a50ba2ead38ef3809b
Mahmoud Abdelhafez <mahmoud.abdelhafz249@gmail.com>
2024-07-28 11:01:45 +0300

    Update
    Update

--- Restaurant-Desktop-Application/Restaurant/obj/Release/net8.0-windows/Restaurant.AssemblyInfo.cs
+++ Restaurant-Desktop-Application/Restaurant/obj/Release/net8.0-windows/Restaurant.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Restaurant")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06c5fb069cb7673be2294f5cf5e5a8992fd0b8a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+607be173afa1a3cd3e0dcb555e156f685b817fcf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Restaurant")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Restaurant")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
